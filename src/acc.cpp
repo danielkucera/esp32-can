d7433e369c22e0af0cb1ebd49c7206852622c84c
Daniel Kucera <daniel.kucera@gmail.com>
2022-10-09 16:57:48 +0200

    working remote gra

--- src/acc.cpp
+++ src/acc.cpp
@@ -3,7 +3,19 @@
 ACC_System acc_message;
 GRA_Neu gra_message;
 
+enum action {
+  DIST_PLUS = 1,
+  DIST_MINUS,
+  SPEED_PLUS,
+  SPEED_MINUS,
+  ACC_SET,
+  ACC_RESET
+};
+
 bool acc_trigger = 0;
+
+int override_gra = 0;
+int override_times = 0;
 
 void trigger_acc_status() {
   acc_trigger = 1;
@@ -63,29 +75,85 @@ void send_gra() {
   gra_message.B.GRA_Neu_Zaehler++;
   gra_message.B.GRA_Checksum = data[1] ^ data[2] ^ data[3];
 
-  int ret = 1;
-  while(ret){
-    int ret = can_send(GRA_NEU_ID, gra_message.U, 4, 1000);
+  int ret = can_send(GRA_NEU_ID, gra_message.U, 4, 20);
+}
+
+void notify_gra(){
+  if (override_gra) {
+    switch (override_gra) {
+      case DIST_MINUS:
+        gra_message.B.GRA_Zeitluecke = 1;
+        printf("overriding GRA ZL1\n");
+        break;
+      case DIST_PLUS:
+        gra_message.B.GRA_Zeitluecke = 2;
+        printf("overriding GRA ZL2\n");
+        break;
+      case SPEED_PLUS:
+        gra_message.B.GRA_Up_kurz = 1;
+        printf("overriding GRA UP\n");
+        break;
+      case SPEED_MINUS:
+        gra_message.B.GRA_Down_kurz = 1;
+        printf("overriding GRA DOWN\n");
+        break;
+      case ACC_SET:
+        gra_message.B.GRA_Neu_Setzen = 1;
+        printf("overriding GRA ACC SET\n");
+        break;
+      case ACC_RESET:
+        gra_message.B.GRA_Recall = 1;
+        printf("overriding GRA ACC RESET\n");
+        break;
+    }
+
+    send_gra();
+
+    override_times--;
+    if (!override_times){
+      override_gra = 0;
+    }
+
+
+    printf("overriden GRA\n");
   }
+
+}
+
+int set_override(int value){
+  override_gra = value;
+  override_times = 6;
+  return 1;
 }
 
 bool keypress_acc(uint32_t ch){
     switch (ch){
       case 'm':
-      case 0xA659FF00:
-        Serial.printf("DIST_MINUS\n");
-        override_gra = DIST_MINUS;
-        notify_gra();
-        //override_times = 2;
-        return 1;
+      case BTN_LEFT:
+        printf("DIST_MINUS\n");
+        return set_override(DIST_MINUS);
         break;
       case 'n':
-      case 0xA956FF00:
-        Serial.printf("DIST_PLUS\n");
-        override_gra = DIST_PLUS;
-        notify_gra();
-        //override_times = 3;
-        return 1;
+      case BTN_RIGHT:
+        printf("DIST_PLUS\n");
+        return set_override(DIST_PLUS);
+        break;
+      case BTN_VOL_PLUS:
+        printf("SPEED_PLUS\n");
+        return set_override(SPEED_PLUS);
+        break;
+      case BTN_VOL_MINUS:
+        printf("SPEED_MINUS\n");
+        return set_override(SPEED_MINUS);
+        break;
+      case BTN_PICKUP:
+        printf("ACC_SET\n");
+        return set_override(ACC_SET);
+        break;
+      case BTN_HANGUP:
+      case BTN_SEL:
+        printf("ACC_RESET\n");
+        return set_override(ACC_RESET);
         break;
     }
     return 0;
